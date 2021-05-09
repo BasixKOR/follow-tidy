391d5a34daf8cbf817f9fb3a9928d062412e9ce0
Basix <basix@basix.tech>
2021-05-09 19:36:20 +0900

    use nextjs instead of vite

---
+++ frontend/pages/api/users/index.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { sampleUserData } from '../../../utils/sample-data';
+
+const handler = (_request: NextApiRequest, response: NextApiResponse) => {
+	try {
+		if (!Array.isArray(sampleUserData)) {
+			throw new TypeError('Cannot find user data');
+		}
+
+		response.status(200).json(sampleUserData);
+	} catch (error: unknown) {
+		if (error instanceof Error)
+			response.status(500).json({ statusCode: 500, message: error.message });
+		else response.status(500).end();
+	}
+};
+
+export default handler;
